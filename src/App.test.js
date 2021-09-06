2ee4adf736321dbd8857c4cd56547cdfdd112918
andrei-iacob <andrei.iacob@learning-panda.com>
2021-09-06 19:54:37 +0100

    making test work

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
+test('renders i\'m a homepage', () => {
+  render(<BrowserRouter><App /></BrowserRouter>);
   const linkElement = screen.getByText(/I'm a homepage/i);
   expect(linkElement).toBeInTheDocument();
 });
